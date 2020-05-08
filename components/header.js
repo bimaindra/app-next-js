93e6d3015d1a33276e54778bc6910f8ce452b995
wongdarjo <bimaindramulya@gmail.com>
2020-05-08 10:53:34 +0700

    chore: update

--- components/header.js
+++ components/header.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 const Style = {
@@ -8,20 +9,30 @@ const Style = {
 
 const Header = () => {
     return (
-        <ul style={Style}>
-            <li style={Style}>
-                <Link href="/"><a>Home</a></Link>
-            </li>
-            <li style={Style}>
-                <Link href="/about"><a>About</a></Link>
-            </li>
-            <li style={Style}>
-                <Link href="/article"><a>News</a></Link>
-            </li>
-            <li style={Style}>
-                <Link href="/show"><a>TV Show</a></Link>
-            </li>
-        </ul>
+        <div>
+            <Head>
+                <title>Nex Js Test | by Bima Indra</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta name="description" content="This is an example of a meta description. This will often show up in search results."></meta>
+            </Head>
+            <ul style={Style}>
+                <li style={Style}>
+                    <Link href="/"><a>Home</a></Link>
+                </li>
+                <li style={Style}>
+                    <Link href="/about"><a>About</a></Link>
+                </li>
+                <li style={Style}>
+                    <Link href="/article"><a>News</a></Link>
+                </li>
+                <li style={Style}>
+                    <Link href="/show"><a>TV Show</a></Link>
+                </li>
+                <li style={Style}>
+                    <Link href="/kita-bisa"><a>KitaBisa</a></Link>
+                </li>
+            </ul>
+        </div>
     )
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import Layout from '../components/Layout';
 
+let looping = (number) => {
+
+    for (let i = 0; i < number.length; i++) {
+        
+    }
+
+    console.log(i);
+
+}
+
 const Index = () => {
 	return (
         <Layout>
             <h1>Homepage</h1>
             <p>This is in homepage</p>
+            <br/>
+            <br/>
+            <br/>
+            <p>Test Looping</p>
+            <div id="loop"></div>
         </Layout>
 	)
 };
